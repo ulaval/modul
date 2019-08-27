77a37ae874044e6e0ae4efbe8f9b890e1464821f
Jacob Masson <Jacob.Masson@dti.ulaval.ca>
2019-08-27 15:23:40 -0400

    MODUL-1178 - ajouter des slots au composant breadcrumbs

--- packages/modul-components/src/components/breadcrumbs/breadcrumbs.stories.ts
+++ packages/modul-components/src/components/breadcrumbs/breadcrumbs.stories.ts
@@ -5,11 +5,20 @@ import VueRouter from 'vue-router';
 
 import { componentsHierarchyRootSeparator } from '../../../conf/storybook/utils';
 import { BREADCRUMBS_NAME } from '../component-names';
+import IconPlugin from '../icon/icon';
 import LinkPlugin from '../link/link';
 import BreadcrumbsPlugin, { BreadcrumbItem, MBreadcrumbs } from './breadcrumbs';
 
 
+
+
+
+
+
+
+
 Vue.use(BreadcrumbsPlugin);
+Vue.use(IconPlugin);
 Vue.use(LinkPlugin);
 Vue.use(VueRouter);
 
@@ -65,7 +74,7 @@ const itemWithoutUrl: BreadcrumbItem = {
     url: ''
 };
 
-const breadcrumbItems: BreadcrumbItem[] = [itemWithDivider, itemDisabled, itemWithoutText, itemWithIcon, itemWithoutUrl, itemWithoutUrl, itemWithoutUrl, itemWithoutUrl];
+const breadcrumbItems: BreadcrumbItem[] = [itemWithDivider, itemDisabled, itemWithoutText, itemWithIcon, itemWithoutUrl];
 
 storiesOf(`${componentsHierarchyRootSeparator}${BREADCRUMBS_NAME}`, module)
     .addDecorator(storyRouterDecorator())
@@ -80,4 +89,57 @@ storiesOf(`${componentsHierarchyRootSeparator}${BREADCRUMBS_NAME}`, module)
             items: breadcrumbItems
         }),
         template: `<m-breadcrumbs :items="items" :divider="divider"/>`
+    }))
+    .add('Custom divider', () => ({
+        components: { MBreadcrumbs },
+        props: {
+            divider: {
+                default: text('Divider', '$')
+            }
+        },
+        data: () => ({
+            items: breadcrumbItems
+        }),
+        template: `<m-breadcrumbs :items="items" :divider="divider"/>`
+    }))
+    .add('Disabled', () => ({
+        components: { MBreadcrumbs },
+        props: {
+            divider: {
+                default: text('Divider', '/')
+            }
+        },
+        data: () => ({
+            items: breadcrumbItems
+        }),
+        template: `<m-breadcrumbs disabled="true" :items="items" :divider="divider"/>`
+    }))
+    .add('Divider Slot', () => ({
+        components: { MBreadcrumbs },
+        props: {
+            divider: {
+                default: text('Divider', '/')
+            }
+        },
+        data: () => ({
+            items: breadcrumbItems
+        }),
+        template: `<m-breadcrumbs :items="items" :divider="divider">
+                        <template #divider><m-icon name="m-svg__close-clear"></m-icon></template>
+                    </m-breadcrumbs>`
+    }))
+    .add('Breadcrumb-item Slot', () => ({
+        components: { MBreadcrumbs },
+        props: {
+            divider: {
+                default: text('Divider', '/')
+            }
+        },
+        data: () => ({
+            items: breadcrumbItems,
+            itemWithIcon
+        }),
+        template: `<m-breadcrumbs :items="items" :divider="divider">
+                        <template v-slot:item="{ item }"><m-button icon-name="m-svg__close-clear" skin="secondary">{{ item.text }}</m-button></template>
+                    </m-breadcrumbs>`
     }));
