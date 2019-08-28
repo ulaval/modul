a8ff3822d24f79bafd49cd51bab75b1914755642
Charles Maheu <choc.mah@gmail.com>
2019-08-28 10:23:34 -0400

    WIP fixed header

--- src/modul-website/src/components/mega-menu/mega-menu.ts
+++ src/modul-website/src/components/mega-menu/mega-menu.ts
@@ -1,6 +1,6 @@
 // import MetaAll from '@/meta/meta-all';
 import { ComponentMeta } from '@/content/components.meta.loader';
-import { ROUTER_PHILOSOPHY, ROUTER_STANDARDS_ACCESSIBILITY, ROUTER_STANDARDS_DEVELOPMENT, ROUTER_STANDARDS_EDITORIAL, ROUTER_STANDARDS_UI } from '@/router';
+import { ROUTER_PHILOSOPHY, ROUTER_STANDARDS_ACCESSIBILITY, ROUTER_STANDARDS_ACCESSIBILITY_CHEATSHEET, ROUTER_STANDARDS_ACCESSIBILITY_IMPLEMENTATION, ROUTER_STANDARDS_ACCESSIBILITY_WHY, ROUTER_STANDARDS_DEVELOPMENT, ROUTER_STANDARDS_DEVELOPMENT_CSS_SASS, ROUTER_STANDARDS_DEVELOPMENT_TYPESCRIPT, ROUTER_STANDARDS_EDITORIAL, ROUTER_STANDARDS_EDITORIAL_FORMAT, ROUTER_STANDARDS_EDITORIAL_GLOSSARY, ROUTER_STANDARDS_EDITORIAL_MESSAGE_BANK, ROUTER_STANDARDS_EDITORIAL_MESSAGE_PUNCTUATION, ROUTER_STANDARDS_EDITORIAL_TONE, ROUTER_STANDARDS_UI, ROUTER_STANDARDS_UI_BREAKPOINTS, ROUTER_STANDARDS_UI_COLORS, ROUTER_STANDARDS_UI_ICONOGRAPHY, ROUTER_STANDARDS_UI_TYPOGRAPHY } from '@/router';
 import IconButtonPlugin from '@ulaval/modul-components/dist/components/icon-button/icon-button';
 import { MediaQueries } from '@ulaval/modul-components/dist/mixins/media-queries/media-queries';
 import { PluginObject } from 'vue';
@@ -9,9 +9,15 @@ import { Prop, Watch } from 'vue-property-decorator';
 import { ModulWebsite } from '../modul-website';
 import WithRender from './mega-menu.html?style=./mega-menu.scss';
 
+type Page = {
+    id: string;
+    text: string;
+};
+
 type Category = {
     id: string;
     text: string;
+    pages?: Page[];
 };
 
 @WithRender
@@ -43,21 +49,85 @@ export class MWMegaMenu extends ModulWebsite {
         this.pagesStandards.push(
             {
                 id: ROUTER_STANDARDS_UI,
-                text: this.$i18n.translate(`website:standards-ui`)
+                text: this.$i18n.translate(`website:standards-ui`),
+                pages: [
+                    {
+                        id: ROUTER_STANDARDS_UI_COLORS,
+                        text: this.$i18n.translate('website:standards-colors-and-themes')
+                    },
+                    {
+                        id: ROUTER_STANDARDS_UI_ICONOGRAPHY,
+                        text: this.$i18n.translate('website:standards-iconography')
+                    },
+                    {
+                        id: ROUTER_STANDARDS_UI_TYPOGRAPHY,
+                        text: this.$i18n.translate('website:standards-typography')
+                    },
+                    {
+                        id: ROUTER_STANDARDS_UI_BREAKPOINTS,
+                        text: this.$i18n.translate('website:standards-breakpoints')
+                    }
+                ]
             },
             {
                 id: ROUTER_STANDARDS_EDITORIAL,
-                text: this.$i18n.translate(`website:standards-editorial`)
+                text: this.$i18n.translate(`website:standards-editorial`),
+                pages: [
+                    {
+                        id: ROUTER_STANDARDS_EDITORIAL_TONE,
+                        text: this.$i18n.translate('website:standards-editorial-tone')
+                    },
+                    {
+                        id: ROUTER_STANDARDS_EDITORIAL_FORMAT,
+                        text: this.$i18n.translate('website:standards-editorial-format')
+                    },
+                    {
+                        id: ROUTER_STANDARDS_EDITORIAL_GLOSSARY,
+                        text: this.$i18n.translate('website:standards-editorial-glossary')
+                    },
+                    {
+                        id: ROUTER_STANDARDS_EDITORIAL_MESSAGE_BANK,
+                        text: this.$i18n.translate('website:standards-editorial-message-bank')
+                    },
+                    {
+                        id: ROUTER_STANDARDS_EDITORIAL_MESSAGE_PUNCTUATION,
+                        text: this.$i18n.translate('website:standards-editorial-punctuation')
+                    }
+                ]
             }
             ,
             {
                 id: ROUTER_STANDARDS_DEVELOPMENT,
-                text: this.$i18n.translate(`website:standards-development`)
+                text: this.$i18n.translate(`website:standards-development`),
+                pages: [
+                    {
+                        id: ROUTER_STANDARDS_DEVELOPMENT_CSS_SASS,
+                        text: this.$i18n.translate('website:standards-development-css-sass')
+                    },
+                    {
+                        id: ROUTER_STANDARDS_DEVELOPMENT_TYPESCRIPT,
+                        text: this.$i18n.translate('website:standards-development-typescript')
+                    }
+                ]
             }
             ,
             {
                 id: ROUTER_STANDARDS_ACCESSIBILITY,
-                text: this.$i18n.translate(`website:accessibility-standards`)
+                text: this.$i18n.translate(`website:accessibility-standards`),
+                pages: [
+                    {
+                        id: ROUTER_STANDARDS_ACCESSIBILITY_WHY,
+                        text: this.$i18n.translate('website:website:accessibility-standards-why')
+                    },
+                    {
+                        id: ROUTER_STANDARDS_ACCESSIBILITY_CHEATSHEET,
+                        text: this.$i18n.translate('website:accessibility-standards-cheatsheet')
+                    },
+                    {
+                        id: ROUTER_STANDARDS_ACCESSIBILITY_IMPLEMENTATION,
+                        text: this.$i18n.translate('website:accessibility-standards-implementation')
+                    }
+                ]
             }
         );
 
