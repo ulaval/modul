2dcefbda120ddda7faa46867f95b156e16f0acd5
mounir-ul <52465144+mounir-ul@users.noreply.github.com>
2019-09-05 13:45:27 -0400

    Feature/phonefield detect country (#27)
    * Detect the country from a given phone number
    
    * Rename constant
    
    * detect country storybook
    
    * update phonefield snapshot
    
    * improve parsePhoneNumber calling method
    
    * snapshot update
    
    * remove beforeMount()
    
    * fix snapshot

--- packages/modul-components/src/components/phonefield/phonefield.stories.ts
+++ packages/modul-components/src/components/phonefield/phonefield.stories.ts
@@ -17,6 +17,27 @@ storiesOf(`${componentsHierarchyRootSeparator}${PHONEFIELD_NAME}`, module)
         }),
         template: '<div><m-phonefield v-model="model" :country.sync="country"></m-phonefield><p>v-model = {{model}}</p><p>country selected = {{JSON.stringify(country)}}</p></div>'
     }))
+    .add('detect country', () => ({
+        data: () => ({
+            model: '+14188753893',
+            country: {},
+            options: ['+7 (800) 555-35-35', '+1 212 201-2700', '+1 4188753893', '+1(213) 373-4253']
+        }),
+        template: `<div>
+                        <div>
+                            <p>Select a phone number</p>
+                            <m-select :options="options" v-model="model">
+                                <template slot="selection">{{ model }}</template>
+                                <template slot="option" slot-scope="{option, index}"> {{ option }} </template>
+                            </m-select>
+                        </div>
+                        <div>
+                            <m-phonefield v-model="model" :country.sync="country"></m-phonefield>
+                            <p>v-model = {{model}}</p>
+                            <p>country selected = {{JSON.stringify(country)}}</p>
+                        </div>
+                    </div>`
+    }))
     .add('disabled', () => ({
         template: '<m-phonefield :disabled="true"></m-phonefield>'
     }))
