5fd55df2c2e79682ac7bc7876faeeece16a307d9
Raphaël Paré <raph.pare@outlook.com>
2019-09-11 11:59:53 -0400

    Améliorer la réactivité d'affichage des $slots du composant m-panel

--- packages/modul-components/src/components/panel/panel.sandbox.ts
+++ packages/modul-components/src/components/panel/panel.sandbox.ts
@@ -1,5 +1,6 @@
 import Vue, { PluginObject } from 'vue';
 import { Component } from 'vue-property-decorator';
+import { MButtonSkin } from '../button/button';
 import { PANEL_NAME } from '../component-names';
 import PanelPlugin from './panel';
 import WithRender from './panel.sandbox.html';
@@ -8,6 +9,13 @@ import WithRender from './panel.sandbox.html';
 @WithRender
 @Component
 export class MPanelSandbox extends Vue {
+    public hasHeader: boolean = true;
+    public hasBody: boolean = true;
+    public hasFooter: boolean = true;
+
+    public getSkinButton(value: boolean): string {
+        return value ? MButtonSkin.Primary : MButtonSkin.Secondary;
+    }
 }
 
 const PanelSandboxPlugin: PluginObject<any> = {
