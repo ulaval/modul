d508aeac06b9dd9f8e12ed3969581e58f0824a0c
Charles Maheu <choc.mah@gmail.com>
2019-09-12 16:02:57 -0400

    ElementQueries.init

--- src/modul-website/src/pages/philosophy/philosophy.ts
+++ src/modul-website/src/pages/philosophy/philosophy.ts
@@ -1,4 +1,5 @@
 
+import { ROUTER_STANDARDS_UI_BREAKPOINTS } from '@/router';
 import { ModulVue } from '@ulaval/modul-components/dist/utils/vue/vue';
 import Component from 'vue-class-component';
 import WithRender from './philosophy.html?style=./philosophy.scss';
@@ -6,4 +7,8 @@ import WithRender from './philosophy.html?style=./philosophy.scss';
 @WithRender
 @Component
 export class MWPhilosophyPage extends ModulVue {
+
+    public get breakpoints(): any {
+        return { name: ROUTER_STANDARDS_UI_BREAKPOINTS };
+    }
 }
