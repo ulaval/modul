c3b1dc66c68d1055a99b7b340745258245628c0f
Jean-Philippe Guilmette <jean-philippe.guilmette@dti.ulaval.ca>
2019-09-19 13:31:52 -0400

    Tests

--- packages/modul-components/src/components/multi-select/multi-select.stories.ts
+++ packages/modul-components/src/components/multi-select/multi-select.stories.ts
@@ -10,6 +10,7 @@ Vue.use(MultiSelectPlugin);
 const optionsVonTrapp: string[] = ['Maria Rainer', 'Georg von Trapp', 'Liesl von Trapp', 'Friedrich von Trapp', 'Louisa von Trapp', 'Kurt von Trapp', 'Brigitta von Trapp', 'Marta von Trapp', 'Gretl von Trapp'];
 const optionsLionKing: string[] = ['Simba', 'Mufasa', 'Scar', 'Timon', 'Pumbaa', 'Rafiki', 'Nala', 'Zazu', 'Shenzi', 'Banzaï', 'Ed', 'Sarabi', 'Sarafina'];
 const optionsPawPatrol: string[] = ['Marcus', 'Ruben', 'Chase', 'Rocky', 'Zuma', 'Stella', 'Everest', 'Tracker', 'Ryder', 'Jake', 'Mairesse Goodway', 'Maire Hollinger'];
+const optionsColor: String[] = ['AliceBlue', 'AntiqueWhite', 'Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque', 'Black', 'BlanchedAlmond', 'Blue', 'BlueViolet', 'Brown', 'BurlyWood', 'CadetBlue', 'Chartreuse', 'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan', 'DarkBlue', 'DarkCyan', 'DarkGoldenRod', 'DarkGray', 'DarkGreen', 'DarkGrey', 'DarkKhaki', 'DarkMagenta', 'DarkOliveGreen', 'DarkOrange', 'DarkOrchid', 'DarkRed', 'DarkSalmon', 'DarkSeaGreen', 'DarkSlateBlue', 'DarkSlateGray', 'DarkSlateGrey', 'DarkTurquoise', 'DarkViolet', 'DeepPink', 'DeepSkyBlue', 'DimGray', 'DimGrey', 'DodgerBlue', 'FireBrick', 'FloralWhite', 'ForestGreen', 'Fuchsia', 'Gainsboro', 'GhostWhite', 'Gold', 'Goldenrod', 'Gray', 'Green', 'GreenYellow', 'Grey', 'HoneyDew', 'HotPink', 'IndianRed', 'Indigo', 'Ivory', 'Khaki', 'Lavender', 'LavenderBlush', 'LawnGreen', 'LemonChiffon', 'LightBlue', 'LightCoral', 'LightCyan', 'LightGoldenRodYellow', 'LightGray', 'LightGreen', 'LightGrey', 'LightPink', 'LightSalmon', 'LightSeaGreen', 'LightSkyBlue', 'LightSlateGray', 'LightSlateGrey', 'LightSteelBlue', 'LightYellow', 'Lime', 'LimeGreen', 'Linen', 'Magenta', 'Maroon', 'MediumAquaMarine', 'MediumBlue', 'MediumOrchid', 'MediumPurple', 'MediumSeaGreen', 'MediumSlateBlue', 'MediumSpringGreen', 'MediumTurquoise', 'MediumVioletRed', 'MidnightBlue', 'MintCream', 'MistyRose', 'Moccasin', 'NavajoWhite', 'Navy', 'OldLace', 'Olive', 'OliveDrab', 'Orange', 'OrangeRed', 'Orchid', 'PaleGoldenRod', 'PaleGreen', 'PaleTurquoise', 'PaleVioletRed', 'PapayaWhip', 'PeachPuff', 'Peru', 'Pink', 'Plum', 'PowderBlue', 'Purple', 'RebeccaPurple', 'Red', 'RosyBrown', 'RoyalBlue', 'SaddleBrown', 'Salmon', 'SandyBrown', 'SeaGreen', 'SeaShell', 'Sienna', 'Silver', 'SkyBlue', 'SlateBlue', 'SlateGray', 'SlateGrey', 'Snow', 'SpringGreen', 'SteelBlue', 'Tan', 'Teal', 'Thistle', 'Tomato', 'Turquoise', 'Violet', 'Wheat', 'White', 'WhiteSmoke', 'Yellow', 'YellowGreen'];
 
 storiesOf(`${componentsHierarchyRootSeparator}${MULTI_SELECT_NAME}`, module)
     .add('default', () => ({
@@ -24,12 +25,32 @@ storiesOf(`${componentsHierarchyRootSeparator}${MULTI_SELECT_NAME}`, module)
             model1: [optionsVonTrapp[0]],
             options: optionsVonTrapp
         }),
-        template: `<div><m-multi-select @open="open" @close="close" @focus="focus" @blur="blur" @select-item="select" :options="options" v-model="model1"></m-multi-select> </div>`
+        template: `<m-multi-select @open="open" @close="close" @focus="focus" @blur="blur" @select-item="select" :options="options" v-model="model1"></m-multi-select>`
     })
     );
 
 storiesOf(`${componentsHierarchyRootSeparator}${MULTI_SELECT_NAME}`, module)
-    .add('Label', () => ({
+    .add('complete', () => ({
+        methods: actions(
+            'open',
+            'close',
+            'focus',
+            'blur',
+            'select'
+        ),
+        data: () => ({
+            model1: [],
+            options: optionsColor
+        }),
+        template: `<m-multi-select @open="open" @close="close" @focus="focus" @blur="blur" @select-item="select" :options="options" v-model="model1" label="CSS Colors">
+            <template v-slot:chips="{item , index }"><div style="display: inline-block; width: 8px; height: 8px; margin-right: 4px;" :style="{ background: item }"></div> {{ item }}</template>
+            <template v-slot:items="{item , index }">{{ index }} - <div style="display: inline-block; width: 8px; height: 8px; margin-right: 4px;" :style="{ background: item }"></div> {{ item }}</template>
+        </m-multi-select>`
+    })
+    );
+
+storiesOf(`${componentsHierarchyRootSeparator}${MULTI_SELECT_NAME}`, module)
+    .add('label', () => ({
         methods: actions(
             'open',
             'close',
@@ -41,12 +62,12 @@ storiesOf(`${componentsHierarchyRootSeparator}${MULTI_SELECT_NAME}`, module)
             model1: [optionsVonTrapp[0]],
             options: optionsVonTrapp
         }),
-        template: `<div><m-multi-select @open="open" @close="close" @focus="focus" @blur="blur" @select-item="select" label="La famille Von Trapp" :options="options" v-model="model1"></m-multi-select> </div>`
+        template: `<m-multi-select @open="open" @close="close" @focus="focus" @blur="blur" @select-item="select" label="La famille Von Trapp" :options="options" v-model="model1"></m-multi-select>`
     })
     );
 
 storiesOf(`${componentsHierarchyRootSeparator}${MULTI_SELECT_NAME}`, module)
-    .add('Custom select-item', () => ({
+    .add('custom select-item', () => ({
         methods: actions(
             'open',
             'close',
@@ -58,12 +79,12 @@ storiesOf(`${componentsHierarchyRootSeparator}${MULTI_SELECT_NAME}`, module)
             model1: [optionsLionKing[0]],
             options: optionsLionKing
         }),
-        template: `<div><m-multi-select @open="open" @close="close" @focus="focus" @blur="blur" @select-item="select" :options="options" v-model="model1"><template v-slot:items="{item , index }"> {{ index }} - {{ item }} </template></m-multi-select> </div>`
+        template: `<m-multi-select @open="open" @close="close" @focus="focus" @blur="blur" @select-item="select" :options="options" v-model="model1"><template v-slot:items="{item , index }"> {{ index }} - {{ item }} </template></m-multi-select>`
     })
     );
 
 storiesOf(`${componentsHierarchyRootSeparator}${MULTI_SELECT_NAME}`, module)
-    .add('Custom chip info', () => ({
+    .add('custom chip info', () => ({
         methods: actions(
             'open',
             'close',
@@ -75,12 +96,12 @@ storiesOf(`${componentsHierarchyRootSeparator}${MULTI_SELECT_NAME}`, module)
             model1: [optionsLionKing[0]],
             options: optionsLionKing
         }),
-        template: `<div><m-multi-select @open="open" @close="close" @focus="focus" @blur="blur" @select-item="select" :options="options" v-model="model1"><template v-slot:chips="{item , index }"> {{ index }} - {{ item }} </template></m-multi-select> </div>`
+        template: `<m-multi-select @open="open" @close="close" @focus="focus" @blur="blur" @select-item="select" :options="options" v-model="model1"><template v-slot:chips="{item , index }"> {{ index }} - {{ item }} </template></m-multi-select>`
     })
     );
 
 storiesOf(`${componentsHierarchyRootSeparator}${MULTI_SELECT_NAME}`, module)
-    .add('Select all', () => ({
+    .add('select all', () => ({
         methods: actions(
             'open',
             'close',
@@ -92,7 +113,7 @@ storiesOf(`${componentsHierarchyRootSeparator}${MULTI_SELECT_NAME}`, module)
             model1: [optionsVonTrapp[0]],
             options: optionsVonTrapp
         }),
-        template: `<div><m-multi-select :select-all="true" @open="open" @close="close" @focus="focus" @blur="blur" @select-item="select" :options="options" v-model="model1"></m-multi-select> </div>`
+        template: `<m-multi-select :select-all="true" @open="open" @close="close" @focus="focus" @blur="blur" @select-item="select" :options="options" v-model="model1"></m-multi-select>`
     })
     );
 
@@ -103,7 +124,7 @@ storiesOf(`${componentsHierarchyRootSeparator}${MULTI_SELECT_NAME}/mobile`, modu
             model1: [],
             options: optionsPawPatrol
         }),
-        template: `<div><m-multi-select :options="options" v-model="model1"></m-multi-select></div>`
+        template: `<m-multi-select :options="options" v-model="model1"></m-multi-select>`
     })
     );
 
@@ -114,6 +135,6 @@ storiesOf(`${componentsHierarchyRootSeparator}${MULTI_SELECT_NAME}/mobile`, modu
             model1: [],
             options: optionsPawPatrol
         }),
-        template: `<div><m-multi-select :options="options" v-model="model1" :select-all="true"></m-multi-select></div>`
+        template: `<m-multi-select :options="options" v-model="model1" :select-all="true"></m-multi-select>`
     })
     );
