6b7298534ded35e6583a400d1aabc2fa77eb9f33
Raphaël Paré <raph.pare@outlook.com>
2019-09-23 16:02:42 -0400

    MAJ

--- packages/modul-components/src/components/rich-text-editor/rich-text-editor.ts
+++ packages/modul-components/src/components/rich-text-editor/rich-text-editor.ts
@@ -106,7 +106,6 @@ export class MRichTextEditor extends ModulVue implements InputManagementData, In
     @Emit('fullscreen')
     onFullscreen(fullscreenWasActived: boolean): void { }
 
-
     public $refs: {
         input: HTMLElement
     };
