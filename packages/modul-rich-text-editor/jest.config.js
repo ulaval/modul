9bb6560f695ae6469fd3f53f6f0fd3c26565a47a
Charles Maheu <choc.mah@gmail.com>
2019-10-16 11:43:54 -0400

    WIP fix build

--- packages/modul-rich-text-editor/jest.config.js
+++ packages/modul-rich-text-editor/jest.config.js
@@ -4,7 +4,11 @@ const { jsWithBabel: tsjPreset } = require('ts-jest/presets');
 
 module.exports = {
     testURL: 'http://localhost/',
-
+    globals: {
+        'ts-jest': {
+            diagnostics: false
+        }
+    },
     roots: [
         '<rootDir>/src/'
     ],
