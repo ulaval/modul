15710f087810fb3fd154fd1ee2e69974b4ca9031
Alvéric Alie <alveric.alie@dti.ulaval.ca>
2019-10-17 15:01:31 -0400

    MODUL-1200 - storybook address

--- src/storybook/src/modul-components/components/address/address-lookup-field/address-lookup-field.stories.ts
+++ src/storybook/src/modul-components/components/address/address-lookup-field/address-lookup-field.stories.ts
@@ -1,5 +1,6 @@
 import { storiesOf } from '@storybook/vue';
 import AddressPlugin from '@ulaval/modul-components/dist/components/address/address';
+import { MAddressLookupField } from '@ulaval/modul-components/dist/components/address/address-lookup-field/address-lookup-field';
 import { ADDRESS_LOOKUP_FIELD_NAME } from '@ulaval/modul-components/dist/components/component-names';
 import { Address } from '@ulaval/modul-components/dist/utils/address-lookup/address';
 import Vue from 'vue';
@@ -17,6 +18,10 @@ storiesOf(`${modulComponentsHierarchyRootSeparator}/m-address/${ADDRESS_LOOKUP_F
         methods: {
             onRetrieve(value: Address): void {
                 (this as any).retrievedValue = value;
+            },
+            clear(): void {
+                (this as any).retrievedValue = 'nothing';
+                ((this as any).$refs.lookup as MAddressLookupField).clear();
             }
         },
         template: `<div>
@@ -24,8 +29,10 @@ storiesOf(`${modulComponentsHierarchyRootSeparator}/m-address/${ADDRESS_LOOKUP_F
             <${ADDRESS_LOOKUP_FIELD_NAME}
                 :origin="origin"
                 :language="language"
-                @address-retrieved="onRetrieve">
+                @address-retrieved="onRetrieve"
+                ref="lookup">
             </${ADDRESS_LOOKUP_FIELD_NAME}>
             <div>RETRIEVED VALUE: {{ retrievedValue }}</div>
+            <button @click="clear()">Clear</button>
         </div>`
     }));
