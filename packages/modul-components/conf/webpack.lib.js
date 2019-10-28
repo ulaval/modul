a224e3aec596ec4cf761d786c5d159fbaa85fc48
Charles Maheu <choc.mah@gmail.com>
2019-10-28 14:35:12 -0400

    modul distribution

---
+++ packages/modul-components/conf/webpack.lib.js
@@ -0,0 +1,110 @@
+const path = require('path');
+const ContextReplacementPlugin = require("webpack/lib/ContextReplacementPlugin");
+
+function resolve(dir) {
+    return path.join(__dirname, '..', dir);
+}
+
+module.exports = {
+    mode: 'production',
+    devtool: 'source-map',
+    entry: {
+        lib: resolve('src/lib.ts')
+    },
+    output: {
+        library: 'modul',
+        libraryTarget: 'umd',
+        path: resolve('dist'),
+    },
+    externals: {
+        vue: {
+            commonjs: 'vue',
+            commonjs2: 'vue',
+            amd: 'vue',
+            root: 'Vue'
+        }
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.html'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        },
+
+    },
+    module: {
+        rules: [
+            {
+                enforce: 'post',
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                enforce: 'post',
+                test: /\.scss$/,
+                use: ['style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                            plugins: function () {
+                                return [
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                enforce: 'pre',
+                test: /\.scss$/,
+                loader: 'sass-loader',
+                options: {
+                    sourceMap: true,
+                    includePaths: ['./src/styles']
+                }
+            },
+            {
+                test: /\.html$/,
+                loader: 'vue-template-loader',
+                options: {
+                    scoped: true
+                }
+            },
+            {
+                test: /\.ts$/,
+                use: [
+                    {
+                        loader: 'thread-loader',
+                        options: {
+                            workers: require('os').cpus().length - 1
+                        }
+                    },
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            configFile: resolve('tsconfig.lib.json'),
+                            happyPackMode: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.svg$/,
+                loader: 'svg-inline-loader',
+                options: {
+                    removeTags: true,
+                    removingTags: ['desc', 'defs', 'style'],
+                    removeSVGTagAttrs: true
+                }
+            }
+        ]
+    },
+    plugins: [
+        new ContextReplacementPlugin(
+            /moment[\/\\]locale$/,
+            /en-ca|fr-ca/
+        )
+    ]
+};
--- packages/modul-components/src/lib.ts
+++ packages/modul-components/src/lib.ts
@@ -1,29 +1,35 @@
-import Vue from 'vue';
-import '../src/styles/main.scss';
-import ComponentsPlugin from './components';
-import DirectivesPlugin from './directives';
-import FiltersPlugin from './filters';
-import FrenchPlugin from './lang/fr';
+import Vue, { PluginObject } from 'vue';
+import ButtonPlugin from './components/button/button';
+import FrenchPlugin from './lang';
+import EnglishPlugin from './lang/en';
 import UtilsPlugin, { UtilsPluginOptions } from './utils';
 import { FRENCH } from './utils/i18n/i18n';
 import './utils/polyfills';
 import DefaultSpritesPlugin from './utils/svg/default-sprites';
 
 
+export interface ModulComponentPluginOptions {
+    curLang?: string;
+}
 
-Vue.config.productionTip = false;
+export const ModulComponentPlugin: PluginObject<ModulComponentPluginOptions | undefined> = {
+    install(v, options: ModulComponentPluginOptions | undefined = { curLang: FRENCH }): void {
 
-const utilsPluginOptions: UtilsPluginOptions = {
-    i18PluginOptions: {
-        curLang: FRENCH
+        const utilsPluginOptions: UtilsPluginOptions = {
+            i18PluginOptions: {
+                curLang: options.curLang
+            },
+            propagateVueParserErrors: false
+        };
+
+        Vue.use(UtilsPlugin, utilsPluginOptions);
+        Vue.use(FrenchPlugin);
+        Vue.use(EnglishPlugin);
+        Vue.use(DefaultSpritesPlugin);
+        Vue.use(ButtonPlugin);
     }
+
 };
 
-Vue.use(UtilsPlugin, utilsPluginOptions);
-Vue.use(ComponentsPlugin);
-Vue.use(DirectivesPlugin);
-Vue.use(FiltersPlugin);
-
-Vue.use(FrenchPlugin);
-Vue.use(DefaultSpritesPlugin);
+export default ModulComponentPlugin;
 
