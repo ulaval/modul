9f9f035a49b6b59c9b5711fb1a0af720bb32eeac
mounir2 <mounir.hab-arrih@dti.ulaval.ca>
2019-11-15 10:54:52 -0500

    move the check to the toggle function

--- packages/modul-components/src/mixins/portal/portal.ts
+++ packages/modul-components/src/mixins/portal/portal.ts
@@ -167,7 +167,7 @@ export class Portal extends ModulVue implements PortalMixin {
     }
 
     public set propOpen(value: boolean) {
-        if (value !== this.internalOpen && !this.disabled) {
+        if (value !== this.internalOpen) {
             if (value) {
                 this.ensurePortalTargetEl(() => {
                     if (this.portalTargetEl) {
@@ -274,7 +274,7 @@ export class Portal extends ModulVue implements PortalMixin {
     }
 
     private toggle(event: MouseEvent): void {
-        if (event.button !== undefined && event.button === MouseButtons.LEFT) {
+        if (!this.disabled && event.button !== undefined && event.button === MouseButtons.LEFT) {
             this.propOpen = !this.propOpen;
             this.$emit('click', event);
         }
