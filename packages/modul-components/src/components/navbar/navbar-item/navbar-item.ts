573d32574dd9e287caf12483528fd82e779492b7
Vincent Guillemette <vincentguillemette1981@gmail.com>
2019-11-29 16:16:14 -0500

    Bugfix/navbar item resizing optimization (#154)
    * * optimization
    
    * * removed console log

--- packages/modul-components/src/components/navbar/navbar-item/navbar-item.ts
+++ packages/modul-components/src/components/navbar/navbar-item/navbar-item.ts
@@ -8,6 +8,7 @@ import WithRender from './navbar-item.html?style=./navbar-item.scss';
 
 // must be sync with selected css class
 const FAKE_SELECTED_CLASS: string = 'm--is-fake-selected';
+const RESIZING_ELEMENT_WIDTH_SAFETY_COUNT: number = 100;
 
 @WithRender
 @Component
@@ -110,42 +111,84 @@ export class MNavbarItem extends ModulVue {
         }
     }
 
-    private setDimension(): void {
-        let itemEl: HTMLElement = this.$refs.item as HTMLElement;
-        if (itemEl && itemEl.style) {
-            itemEl.style.removeProperty('width');
-            itemEl.style.removeProperty('max-width');
-            itemEl.style.removeProperty('white-space');
+    private _computingHeightFontSizeRatio: boolean = false;
+    private _resizedWhileComputing: boolean = false;
 
-            if (this.isMultiline && ((itemEl.innerText === undefined ? '' : itemEl.innerText).trim().length > 15)) {
-                let itemElComputedStyle: any = window.getComputedStyle(itemEl);
-                let fontSize: number = parseFloat(itemElComputedStyle.getPropertyValue('font-size'));
-                let paddingH: number = parseInt(itemElComputedStyle.getPropertyValue('padding-top'), 10) + parseInt(itemElComputedStyle.getPropertyValue('padding-bottom'), 10);
-                // must subtract the padding, create a infinite loop
-                let itemElHeight: number = itemEl.clientHeight - paddingH;
-                let lines: number = Math.floor(itemElHeight / fontSize);
+    private _computeHeightFontSizeRatio(): void {
+        if (this._computingHeightFontSizeRatio) {
+            this._resizedWhileComputing = true;
+            return;
+        }
 
-                if (lines > 2) {
-                    // use selected class to reserve space for when selected
-                    this.$el.classList.add(FAKE_SELECTED_CLASS);
-                    // create a infinite loop if the parent has 'align-items: stretch'
-                    (this.$parent.$refs.list as HTMLElement).style.alignItems = 'flex-start';
+        this._computingHeightFontSizeRatio = true;
 
-                    do {
-                        itemEl.style.width = itemEl.clientWidth + 1 + 'px'; // increment width
+        const element: HTMLElement = this.$refs.item as HTMLElement;
+        const itemElementComputedStype: CSSStyleDeclaration = window.getComputedStyle(element);
+        const yPadding: number =
+            parseInt(itemElementComputedStype.getPropertyValue('padding-top'), 10)
+            +
+            parseInt(itemElementComputedStype.getPropertyValue('padding-bottom'), 10);
+        const fontSize: number = parseFloat(itemElementComputedStype.getPropertyValue('font-size'));
 
-                        // update values
-                        itemElHeight = itemEl.clientHeight - paddingH;
-                        lines = Math.floor(itemElHeight / fontSize);
-                    } while (lines > 2);
-
-                    // reset styles once completed
-                    this.$el.classList.remove(FAKE_SELECTED_CLASS);
-                    (this.$parent.$refs.list as HTMLElement).style.removeProperty('align-items');
-                }
-            } else {
-                itemEl.style.whiteSpace = 'nowrap';
+        const compute: Function = (
+            stepInPixels: number = 1,
+            safetyCount: number = 0
+        ) => {
+            if (safetyCount >= RESIZING_ELEMENT_WIDTH_SAFETY_COUNT) {
+                return;
             }
+
+            const itemElementHeightWithoutPadding: number = element.clientHeight - yPadding;
+            const ratio: number = Math.floor(itemElementHeightWithoutPadding / fontSize);
+
+            element.style.width = (element.clientWidth + stepInPixels) + 'px';
+
+            if (ratio <= 2) {
+                return;
+            }
+
+            compute(
+                ratio > 5
+                    ? stepInPixels * 2
+                    : ratio === 4
+                        ? Math.floor(stepInPixels * 1.5)
+                        : stepInPixels + 5,
+                ++safetyCount
+            );
+        };
+
+        compute();
+
+        this._computingHeightFontSizeRatio = false;
+
+        if (this._resizedWhileComputing) {
+            this._resizedWhileComputing = false;
+            this._computeHeightFontSizeRatio();
+        }
+    }
+
+    private setDimension(): void {
+        let itemElement: HTMLElement = this.$refs.item as HTMLElement;
+
+        if (!itemElement || !itemElement.style) {
+            return;
+        }
+
+        itemElement.style.removeProperty('width');
+        itemElement.style.removeProperty('max-width');
+        itemElement.style.removeProperty('white-space');
+
+        if (
+            this.isMultiline
+            && (
+                !!itemElement.innerText
+                &&
+                itemElement.innerText.trim().length > 15
+            )
+        ) {
+            this._computeHeightFontSizeRatio();
+        } else {
+            itemElement.style.whiteSpace = 'nowrap';
         }
     }
 }
