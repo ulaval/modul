2e6ba2e05ed639bd40c777f9e065ebd7f966a1a2
Sébastien Turcotte <sebastien.turcotte@dti.ulaval.ca>
2020-02-13 08:40:14 -0500

    Ajout evenement

--- packages/modul-components/src/components/searchfield/searchfield.ts
+++ packages/modul-components/src/components/searchfield/searchfield.ts
@@ -30,6 +30,7 @@ export class MSearchfield extends ModulVue {
         input: HTMLElement
     };
 
+    @Emit('emptyField')
     public onReset(): void {
         this.as<InputManagement>().model = '';
     }
