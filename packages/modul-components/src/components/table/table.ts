8627ad2c5616a7e050a8d5a618ac46391d47bef5
Raphaël Paré <raph.pare@outlook.com>
2020-03-11 12:37:49 -0400

    Ajouter width-placeholder prop on table

--- packages/modul-components/src/components/table/table.ts
+++ packages/modul-components/src/components/table/table.ts
@@ -64,6 +64,19 @@ export class MTable extends ModulVue {
     @Prop({ default: true })
     public rowHighlightedOnHover: boolean;
 
+    @Prop({
+        default: '100%',
+        validator: value => {
+            const pixelOrPercentageNumberRegExp: RegExp = /^\d+(\.[0-9]{1,4})?(px|%)$/;
+            if (!value.search(pixelOrPercentageNumberRegExp)) {
+                // tslint:disable-next-line: no-console
+                console.warn(`width-placeholder value needs to respect this RegEx: ${pixelOrPercentageNumberRegExp}`);
+            }
+            return !value.search(pixelOrPercentageNumberRegExp);
+        }
+    })
+    public widthPlaceholder: string;
+
     public i18nEmptyTable: string = this.$i18n.translate('m-table:no-data');
     public i18nLoading: string = this.$i18n.translate('m-table:loading');
     public i18nPleaseWait: string = this.$i18n.translate('m-table:please-wait');
@@ -78,6 +91,14 @@ export class MTable extends ModulVue {
 
     public get isEmpty(): boolean {
         return this.rows.length === 0 && !this.loading;
+    }
+
+    public get placeholderPositionType(): string {
+        return this.widthPlaceholder.search(/^\d+(\.[0-9]{1,4})?px$/) ? 'absolute' : 'sticky';
+    }
+
+    public get columnsInternal(): MColumnTableInternal[] {
+        return this.columns.map((c: MColumnTable) => ({ ...c }));
     }
 
     public sort(columnTable: MColumnTableInternal): void {
@@ -162,10 +183,6 @@ export class MTable extends ModulVue {
 
     public columnWidth(col: MColumnTable): { width: string } | '' {
         return col.width ? { width: col.width } : '';
-    }
-
-    get columnsInternal(): MColumnTableInternal[] {
-        return this.columns.map((c: MColumnTable) => ({ ...c }));
     }
 }
 
