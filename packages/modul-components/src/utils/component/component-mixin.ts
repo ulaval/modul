5ef94f7909f07a9b020690a49d67e7f80ea0b08b
Charles <choc.mah@gmail.com>
2020-04-09 13:41:20 -0400

    fix private fonction not working

--- packages/modul-components/src/utils/component/component-mixin.ts
+++ packages/modul-components/src/utils/component/component-mixin.ts
@@ -27,49 +27,44 @@ export class MComponentMixin extends Vue {
                 if (!this.$i18n) {
                     throw new Error('You must install I18nPlugin to use the i18n option');
                 }
-                this.setupI18nComponentOptions(this.$options.modul.i18n);
+                const i18nComponentOptions: I18nComponentOptions = this.$options.modul.i18n;
+                for (let key in i18nComponentOptions) {
+                    if ([FRENCH, ENGLISH].includes(key)) {
+                        let msgs: BundleMessagesMap | BundleMessagesMap[] = i18nComponentOptions[key];
+                        if (Array.isArray(msgs)) {
+                            msgs.forEach(msg => {
+                                this.$i18n.addMessages(key, msg);
+                            });
+                        } else {
+                            this.$i18n.addMessages(key, msgs);
+                        }
+                    } else {
+                        throw new Error(`Unknown lang ${key}`);
+                    }
+                }
             }
 
             if (this.$options.modul.sprites) {
                 if (!this.$svg) {
                     throw new Error('You must install SpritesPlugin to use the sprites option');
                 }
-                this.setupSvgComponentOptions(this.$options.modul.sprites);
-            }
-        }
+                const svgComponentOptions: SpritesComponentOptions = this.$options.modul.sprites;
 
-    }
-    private setupSvgComponentOptions(svgComponentOptions: SpritesComponentOptions): void {
-        if (svgComponentOptions.internalSprites) {
-            if (Array.isArray(svgComponentOptions.internalSprites)) {
-                svgComponentOptions.internalSprites.forEach(sprite => {
-                    this.$svg.addInternalSprites(sprite);
-                });
-            } else {
-                this.$svg.addInternalSprites(svgComponentOptions.internalSprites);
-            }
-        } else {
-            throw new Error(`You must provide an internalSprites option`);
-        }
-    }
-
-    private setupI18nComponentOptions(i18nComponentOptions: I18nComponentOptions): void {
-        for (let key in i18nComponentOptions) {
-            if ([FRENCH, ENGLISH].includes(key)) {
-                let msgs: BundleMessagesMap | BundleMessagesMap[] = i18nComponentOptions[key];
-                if (Array.isArray(msgs)) {
-                    msgs.forEach(msg => {
-                        this.$i18n.addMessages(key, msg);
-                    });
+                if (svgComponentOptions.internalSprites) {
+                    if (Array.isArray(svgComponentOptions.internalSprites)) {
+                        svgComponentOptions.internalSprites.forEach(sprite => {
+                            this.$svg.addInternalSprites(sprite);
+                        });
+                    } else {
+                        this.$svg.addInternalSprites(svgComponentOptions.internalSprites);
+                    }
                 } else {
-                    this.$i18n.addMessages(key, msgs);
+                    throw new Error(`You must provide an internalSprites option`);
                 }
-            } else {
-                throw new Error(`Unknown lang ${key}`);
             }
         }
-    }
 
+    }
 }
 
 const ComponentMixinPlugin: PluginObject<any> = {
