ece47e4bb72293d6409ac99bbfcb0a99cf7765a9
Charles Maheu <choc.mah@gmail.com>
2020-04-09 11:49:18 -0400

    Feature/enhancenment component options (#280)
    * WIP enhancement svg
    
    * fix snapshot

--- packages/modul-components/src/utils/component/component-mixin.ts
+++ packages/modul-components/src/utils/component/component-mixin.ts
@@ -4,6 +4,7 @@ import { BundleMessagesMap, ENGLISH, FRENCH } from '../i18n/i18n';
 
 export interface ModulComponentOptions {
     i18n?: I18nComponentOptions;
+    sprites?: SpritesComponentOptions;
 }
 
 declare module 'vue/types/options' {
@@ -12,7 +13,8 @@ declare module 'vue/types/options' {
     }
 }
 
-type I18nComponentOptions = { [key: string]: BundleMessagesMap };
+type I18nComponentOptions = { [key: string]: BundleMessagesMap | BundleMessagesMap[] };
+type SpritesComponentOptions = { internalSprites: string | string[] };
 
 
 @Component
@@ -22,23 +24,50 @@ export class MComponentMixin extends Vue {
         if (this.$options.modul) {
 
             if (this.$options.modul.i18n) {
-
                 if (!this.$i18n) {
                     throw new Error('You must install I18nPlugin to use the i18n option');
                 }
+                this.setupI18nComponentOptions(this.$options.modul.i18n);
+            }
 
-                for (let key in this.$options.modul.i18n) {
-                    if ([FRENCH, ENGLISH].includes(key)) {
-                        let msgs: BundleMessagesMap = this.$options.modul.i18n[key];
-                        this.$i18n.addMessages(key, msgs);
-                    } else {
-                        throw new Error(`Unknown lang ${key}`);
-                    }
-
+            if (this.$options.modul.sprites) {
+                if (!this.$svg) {
+                    throw new Error('You must install SpritesPlugin to use the sprites option');
                 }
+                this.setupSvgComponentOptions(this.$options.modul.sprites);
             }
         }
 
+    }
+    private setupSvgComponentOptions(svgComponentOptions: SpritesComponentOptions): void {
+        if (svgComponentOptions.internalSprites) {
+            if (Array.isArray(svgComponentOptions.internalSprites)) {
+                svgComponentOptions.internalSprites.forEach(sprite => {
+                    this.$svg.addInternalSprites(sprite);
+                });
+            } else {
+                this.$svg.addInternalSprites(svgComponentOptions.internalSprites);
+            }
+        } else {
+            throw new Error(`You must provide an internalSprites option`);
+        }
+    }
+
+    private setupI18nComponentOptions(i18nComponentOptions: I18nComponentOptions): void {
+        for (let key in i18nComponentOptions) {
+            if ([FRENCH, ENGLISH].includes(key)) {
+                let msgs: BundleMessagesMap | BundleMessagesMap[] = i18nComponentOptions[key];
+                if (Array.isArray(msgs)) {
+                    msgs.forEach(msg => {
+                        this.$i18n.addMessages(key, msg);
+                    });
+                } else {
+                    this.$i18n.addMessages(key, msgs);
+                }
+            } else {
+                throw new Error(`Unknown lang ${key}`);
+            }
+        }
     }
 
 }
