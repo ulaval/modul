10f3e11c832fea8a8d2ab38c3819ad73363871f2
sioue19 <35466497+sioue19@users.noreply.github.com>
2020-04-16 10:04:08 -0400

    [f-m-phone] Ajout d'un filtre pour les no de téléphone (#282)
    * Ajout du filtre phone
    
    * Changé condition
    
    * Ajouté storyshot manquant
    
    * Corrigé story qui ne fonctionnait pas
    
    * Modifs de pull request
    
    * Updaté snapshot
    
    * Mis le format international par défaut
    
    * Updaté snapshot
    
    * Updaté snapshot
    
    * Corrigé le code de marde à Max
    
    * Enlevé la modif à Sébastien Turcotte pour la mettre dans sa pull request

---
+++ packages/modul-components/src/filters/phone/phone.ts
@@ -0,0 +1,35 @@
+import { parsePhoneNumberFromString, PhoneNumber } from 'libphonenumber-js';
+import { PluginObject } from 'vue';
+import { PHONE_NAME } from '../filter-names';
+
+/**
+ * Available formats:
+ * NATIONAL — Example: "(213) 373-4253"
+ * INTERNATIONAL — Example: "+1 213 373 4253"
+ *
+ * A ticket has been opened for Google in order to fix the National format. It shouldn't have parenthesis.
+ * https://issuetracker.google.com/issues/153616208)
+ */
+export enum PhoneNumberFormat {
+    INTERNATIONAL = 'INTERNATIONAL',
+    NATIONAL = 'NATIONAL'
+}
+
+export class PhoneFilter {
+    static format(text: string, format: PhoneNumberFormat = PhoneNumberFormat.INTERNATIONAL): string {
+        if (text.length > 0) {
+            const phoneNumber: PhoneNumber | undefined = parsePhoneNumberFromString(text, 'CA');
+            return phoneNumber ? phoneNumber.format(format) : '';
+        } else {
+            return '';
+        }
+    }
+}
+
+const PhoneFilterPlugin: PluginObject<any> = {
+    install(v): void {
+        v.filter(PHONE_NAME, PhoneFilter.format);
+    }
+};
+
+export default PhoneFilterPlugin;
---
+++ src/storybook/src/modul-components/filters/phone/phone.stories.ts
@@ -0,0 +1,42 @@
+import { select, text } from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/vue';
+import { PHONE_NAME } from '@ulaval/modul-components/dist/filters/filter-names';
+import PhoneFilterPlugin, { PhoneFilter, PhoneNumberFormat } from '@ulaval/modul-components/dist/filters/phone/phone';
+import Vue from 'vue';
+import { modulComponentsHierarchyRootSeparator } from '../../../utils';
+
+Vue.use(PhoneFilterPlugin);
+
+function getBaseVueWithLocale(template: string): any {
+    return {
+        template,
+        props: {
+            text: {
+                default: text('Phone number', '+14185479029')
+            },
+            format: {
+                default: select('Format', {
+                    [PhoneNumberFormat.NATIONAL]: PhoneNumberFormat.NATIONAL,
+                    [PhoneNumberFormat.INTERNATIONAL]: PhoneNumberFormat.INTERNATIONAL
+                }, PhoneNumberFormat.INTERNATIONAL)
+            }
+        },
+        methods: {
+            formatedPhoneNumber(text: string, format: PhoneNumberFormat): string {
+                return PhoneFilter.format(text, format);
+            }
+        }
+    };
+}
+
+function getBaseSimpleVue(template: string): any {
+    return getBaseVueWithLocale(template);
+}
+
+storiesOf(`${modulComponentsHierarchyRootSeparator}${PHONE_NAME}`, module)
+    .add('Default', () => getBaseSimpleVue(`
+        <div>
+            <div>Phone number : <span v-html="text"></span></div>
+            <div>Result : <span v-html="formatedPhoneNumber(text, format)"></span></div>
+        </div>`)
+    );
