3828a9ad7cd746e9b1bf0ae702a6fe9444d55436
Charles Maheu <choc.mah@gmail.com>
2020-04-23 13:31:14 -0400

    [m-phonefield] Remove cleave-phone.i18n.js  (#294)
    * remove cleave-phone.i18n.js
    
    * Ajout de l'autocomplete pour les navigateurs
    
    * bloqué saison sur maxlength
    
    * update storybook
    
    * corriger problème affichage
    
    * ajout un min si exemple vide
    
    * enlever la limit
    
    * update snapshot
    
    Co-authored-by: Maxime Turcotte <maxime.turcotte@dti.ulaval.ca>

---
+++ packages/modul-components/src/components/phonefield/formatter/custom-asyoutype.ts
@@ -0,0 +1,18 @@
+import { AsYouType } from 'libphonenumber-js';
+
+export class CustomAsYouType {
+    private asYouType: AsYouType;
+    constructor() {
+        this.asYouType = new AsYouType();
+    }
+
+    public inputDigit(digit: string): string {
+        return this.asYouType.input(digit);
+    }
+
+
+    public clear(): void {
+        return this.asYouType.reset();
+    }
+
+}
