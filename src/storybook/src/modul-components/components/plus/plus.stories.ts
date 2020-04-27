61be27109840297a3ba41603a548a9321a49deb1
Charles <choc.mah@gmail.com>
2020-04-27 11:30:44 -0400

    WIP

---
+++ src/storybook/src/modul-components/components/plus/plus.stories.ts
@@ -0,0 +1,25 @@
+import { PLUS_NAME } from '@ulaval/modul-components/dist/components/component-names';
+import { modulComponentsHierarchyRootSeparator } from '../../../utils';
+
+export default {
+    title: `${modulComponentsHierarchyRootSeparator}${PLUS_NAME}`
+};
+
+export const defaultStory = () => '<m-plus></m-plus>';
+
+defaultStory.story = {
+    name: 'default'
+};
+
+export const open = () => '<m-plus :open="true"></m-plus>';
+
+export const large = () => '<m-plus :large="true"></m-plus>';
+
+export const border = () => '<m-plus :border="true"></m-plus>';
+
+export const disabled = () => '<m-plus :disabled="true"></m-plus>';
+
+export const skinLight = () => ` <div style="background:lightgrey;" >
+ <m-plus skin = "light" > </m-plus></div>` ;
+
+
