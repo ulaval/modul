9fdfbf5bae9cd4cd9fdde0dd2ceed5d0f3b1fe24
Simon Ouellet <simon.ouellet@dti.ulaval.ca>
2020-05-20 10:35:07 -0400

    Ajusté les stories

--- src/storybook/src/modul-components/filters/postal-code/postal-code.stories.ts
+++ src/storybook/src/modul-components/filters/postal-code/postal-code.stories.ts
@@ -1,45 +1,76 @@
-import { select, text } from '@storybook/addon-knobs';
-import { SelectTypeOptionsProp } from '@storybook/addon-knobs/dist/components/types/Select';
-import { storiesOf } from '@storybook/vue';
+import { text } from '@storybook/addon-knobs';
 import { MPostalCodeCountry } from '@ulaval/modul-components/dist/components/postalcodefield/postalcodefield';
 import { POSTAL_CODE_NAME } from '@ulaval/modul-components/dist/filters/filter-names';
-import PostalCodeFilterPlugin, { PostalCodeFilter } from '@ulaval/modul-components/dist/filters/postal-code/postal-code';
+import PostalCodeFilterPlugin from '@ulaval/modul-components/dist/filters/postal-code/postal-code';
 import Vue from 'vue';
 import { modulComponentsHierarchyRootSeparator } from '../../../utils';
 
 Vue.use(PostalCodeFilterPlugin);
 
-function getBaseVueWithLocale(template: string): any {
-    return {
-        template,
-        props: {
-            text: {
-                default: text('Postal Code', 'G2B0B9')
-            },
-            isoCountry: {
-                default: select('ISO Country', getAllCountryOptions(), MPostalCodeCountry.CA)
-            }
-        },
-        methods: {
-            formatedPostalCode(text: string, isoCountry: string): string {
-                return PostalCodeFilter.format(text, isoCountry);
-            }
+let template: string = `<div>{{ text | f-m-postal-code(isoCountry) }}</div>`;
+
+export default {
+    title: `${modulComponentsHierarchyRootSeparator}${POSTAL_CODE_NAME}`,
+    parameters: { fileName: __filename }
+};
+
+export const defaultStory = () => ({
+    props: {
+        text: {
+            default: text('Text', '9999999')
         }
-    };
-}
+    },
+    template: template
+});
 
-function getBaseSimpleVue(template: string): any {
-    return getBaseVueWithLocale(template);
-}
+defaultStory.story = {
+    name: 'Default'
+};
 
-function getAllCountryOptions(): SelectTypeOptionsProp<MPostalCodeCountry> {
-    return MPostalCodeCountry;
-}
+export const ca = () => ({
+    props: {
+        text: {
+            default: text('Text', 'G2B0B9')
+        },
+        isoCountry: {
+            default: MPostalCodeCountry.CA
+        }
+    },
+    template: template
+});
 
-storiesOf(`${modulComponentsHierarchyRootSeparator}${POSTAL_CODE_NAME}`, module)
-    .add('Default', () => getBaseSimpleVue(`
-        <div>
-            <div>Postal Code : <span v-html="text"></span></div>
-            <div>Result : <span v-html="formatedPostalCode(text, isoCountry)"></span></div>
-        </div>`)
-    );
+export const us5Characters = () => ({
+    props: {
+        text: {
+            default: text('Text', '99999')
+        },
+        isoCountry: {
+            default: MPostalCodeCountry.US
+        }
+    },
+    template
+});
+
+export const us9Characters = () => ({
+    props: {
+        text: {
+            default: text('Text', '999999999')
+        },
+        isoCountry: {
+            default: MPostalCodeCountry.US
+        }
+    },
+    template
+});
+
+export const fr5Characters = () => ({
+    props: {
+        text: {
+            default: text('Text', '99999')
+        },
+        isoCountry: {
+            default: MPostalCodeCountry.FR
+        }
+    },
+    template
+});
