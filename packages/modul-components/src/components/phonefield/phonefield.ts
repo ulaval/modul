7ebab08288854d92be487c118576b03cf1129591
Raphaël Paré <raph.pare@outlook.com>
2020-05-20 17:12:19 -0400

    maj

--- packages/modul-components/src/components/phonefield/phonefield.ts
+++ packages/modul-components/src/components/phonefield/phonefield.ts
@@ -1,4 +1,3 @@
-
 import { CountryCode, getExampleNumber, ParsedNumber, parseNumber, PhoneNumber } from 'libphonenumber-js';
 import Component from 'vue-class-component';
 import { Emit, Model, Prop, Watch } from 'vue-property-decorator';
@@ -6,35 +5,21 @@ import { InputLabel } from '../../mixins/input-label/input-label';
 import { InputManagement } from '../../mixins/input-management/input-management';
 import { InputState } from '../../mixins/input-state/input-state';
 import { InputWidth } from '../../mixins/input-width/input-width';
+import { allCountriesOptionsEn } from '../../utils/country/countries-options-en';
+import { allCountriesOptionsFr } from '../../utils/country/countries-options-fr';
+import { MCountry, MCountryCodeISO2, MCountryOptions } from '../../utils/country/country';
 import { FRENCH } from '../../utils/i18n/i18n';
-import { SpritesService } from '../../utils/svg/sprites';
 import uuid from '../../utils/uuid/uuid';
 import { ModulVue } from '../../utils/vue/vue';
+import { MCountryFlag } from '../country-flag/country-flag';
 import { InputMaskOptions, MInputMask } from '../input-mask/input-mask';
 import { MSelectVirtualScroll } from '../select-virtual-scroll/select-virtual-scroll';
 import { MSelectItem } from '../select/select-item/select-item';
-import allCountriesEn from './assets/all-countries-en';
-import allCountriesFr from './assets/all-countries-fr';
 import { CustomAsYouType } from './formatter/custom-asyoutype';
 import WithRender from './phonefield.html?style=./phonefield.scss';
 
 (window as any).Cleave = (window as any).Cleave || {};
 (window as any).Cleave.AsYouTypeFormatter = CustomAsYouType;
-
-interface CountryOptions {
-    name: string;
-    iso2: string;
-    dialCode: string;
-    priority?: number;
-    areaCodes?: string[];
-}
-
-
-export interface Country {
-    iso: string;
-    prefix: string;
-}
-
 
 @WithRender
 @Component({
@@ -47,7 +32,8 @@ export interface Country {
     components: {
         MInputMask,
         MSelectVirtualScroll,
-        MSelectItem
+        MSelectItem,
+        MCountryFlag
     }
 })
 export class MPhonefield extends ModulVue {
@@ -64,12 +50,7 @@ export class MPhonefield extends ModulVue {
             prefix: '1'
         })
     })
-    public country: any;
-
-    @Prop({
-        default: false
-    })
-    public externalSprite: boolean;
+    public country: MCountry;
 
     @Prop({
         default: () => ['ca']
@@ -83,58 +64,59 @@ export class MPhonefield extends ModulVue {
     protected id: string = `mIntegerfield-${uuid.generate()}`;
     private examples: any = require('libphonenumber-js/examples.mobile.json');
 
-
-    countryModelInternal: string = '';
-    internalCountry: CountryOptions = { name: '', iso2: '', dialCode: '' };
-    selectedCountries: CountryOptions[] = this.$i18n.currentLang() === FRENCH ? allCountriesFr : allCountriesEn;
-    countries: CountryOptions[] = this.selectedCountries.sort((a, b) => (this.nameNormalize(a.name) > this.nameNormalize(b.name)) ? 1 : ((this.nameNormalize(b.name) > this.nameNormalize(a.name)) ? -1 : 0));
+    countryModelInternal: MCountryCodeISO2 = MCountryCodeISO2.Empty;
+    internalCountry: MCountryOptions = { name: '', iso2: MCountryCodeISO2.Empty, dialCode: '' };
+    selectedCountries: MCountryOptions[] = this.$i18n.currentLang() === FRENCH ? allCountriesOptionsFr : allCountriesOptionsEn;
+    countries: MCountryOptions[] = this.selectedCountries.sort((a, b) => (this.nameNormalize(a.name) > this.nameNormalize(b.name)) ? 1 : ((this.nameNormalize(b.name) > this.nameNormalize(a.name)) ? -1 : 0));
     internalFocus: boolean = false;
 
     i18nInternalLabel: string = this.$i18n.translate('m-phonefield:phone-label');
     i18nCountryLabel: string = this.$i18n.translate('m-phonefield:country-label');
     i18nExample: string = this.$i18n.translate('m-phonefield:example');
 
-    beforeMount(): void {
-        // sprites-flags.svg is a very big file, this is why sprites should only be added to the DOM before this component is mounted.
-        const sprites: string = require('../../assets/icons/sprites-flags.svg');
-        const svg: SpritesService = this.$svg;
-        if (this.externalSprite) {
-            if (!svg.isInExternalSprites('mflag')) {
-                svg.addExternalSprites(sprites, 'mflag');
-            }
-        } else {
-            if (!document.getElementById('mflag-svg__flag-ae')) {
-                svg.addInternalSprites(sprites);
-            }
-        }
-    }
-
     @Emit('input')
     emitNewValue(_newValue: string): void { }
 
     @Emit('update:country')
-    emitContrySelected(country: Country): void { }
+    emitContrySelected(country: MCountry): void { }
+
+    @Watch('country', { immediate: true })
+    onContryChange(country: MCountry): void {
+        this.countryModelInternal = country.iso;
+        this.internalCountry = this.countries.find((country: MCountryOptions) => country.iso2 === this.countryModelInternal)!;
+        if (!this.as<InputManagement>().internalValue) {
+            this.as<InputManagement>().internalValue = `+${this.internalCountry.dialCode}`;
+        }
+
+    }
+
+    @Watch('value', { immediate: true })
+    onValueChanged(value: string): void {
+        if (value) {
+            this.parsePhoneNumber(value);
+        }
+    }
 
     get isoCountries(): string[] {
         return this.countriesSorted.map(contry => contry.iso2);
     }
 
-    get countriesSorted(): CountryOptions[] {
+    get countriesSorted(): MCountryOptions[] {
 
-        let finalCountriesList: CountryOptions[] = [];
+        let finalCountriesList: MCountryOptions[] = [];
 
         this.priorityIsoCountries.forEach((isoPriorityCountry: string) => {
-            const currentCountry: CountryOptions | undefined = this.countries.find((isoCountry: CountryOptions) => isoCountry.iso2 === isoPriorityCountry);
+            const currentCountry: MCountryOptions | undefined = this.countries.find((isoCountry: MCountryOptions) => isoCountry.iso2 === isoPriorityCountry);
             if (currentCountry) {
                 finalCountriesList.push(currentCountry);
             }
         });
 
         if (this.priorityIsoCountries.length > 0) {
-            finalCountriesList.push({ name: '', iso2: '', dialCode: '' });
+            finalCountriesList.push({ name: '', iso2: MCountryCodeISO2.Empty, dialCode: '' });
         }
 
-        this.countries.filter((country: CountryOptions) => {
+        this.countries.filter((country: MCountryOptions) => {
             if (!this.priorityIsoCountries.includes(country.iso2)) {
                 finalCountriesList.push(country);
             }
@@ -175,12 +157,12 @@ export class MPhonefield extends ModulVue {
         return phoneNumber ? phoneNumber.formatInternational() : '';
     }
 
-    get countryModel(): string {
+    get countryModel(): MCountryCodeISO2 {
         return this.countryModelInternal;
     }
 
-    set countryModel(value: string) {
-        this.internalCountry = this.countries.find((country: CountryOptions) => country.iso2 === value)!;
+    set countryModel(value: MCountryCodeISO2) {
+        this.internalCountry = this.countries.find((country: MCountryOptions) => country.iso2 === value)!;
         this.as<InputManagement>().internalValue = '+' + this.internalCountry.dialCode;
         this.countryModelInternal = value;
     }
@@ -192,8 +174,8 @@ export class MPhonefield extends ModulVue {
     parsePhoneNumber(value: string): void {
         const parsedNumber: ParsedNumber = parseNumber(value, { extended: true }) as ParsedNumber;
         if (parsedNumber.country && parsedNumber.valid) {
-            this.countryModelInternal = parsedNumber.country.toLowerCase();
-            this.internalCountry = this.countries.find((country: CountryOptions) => country.iso2 === this.countryModelInternal)!;
+            this.countryModelInternal = parsedNumber.country.toLowerCase() as MCountryCodeISO2;
+            this.internalCountry = this.countries.find((country: MCountryOptions) => country.iso2 === this.countryModelInternal)!;
             this.emitContrySelected({
                 iso: this.internalCountry.iso2,
                 prefix: this.internalCountry.dialCode
@@ -203,19 +185,6 @@ export class MPhonefield extends ModulVue {
 
     nameNormalize(name: string): string {
         return name.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-    }
-
-    spriteId(iso: string): string | undefined {
-        const svg: SpritesService = this.$svg;
-        const spriteId: string = 'mflag-svg__flag-' + iso;
-
-        if (document.getElementById(spriteId)) {
-            return '#' + spriteId;
-        } else if (svg && svg.getExternalSpritesFromSpriteId(spriteId)) {
-            return svg.getExternalSpritesFromSpriteId(spriteId);
-        } else if (iso) {
-            this.$log.warn('"' + iso + '" is not a valid iso country. Make sure that the sprite has been loaded via the $svg instance service.');
-        }
     }
 
     onContryChanged(countryIso: any, _index: number, _$event: Event): void {
@@ -239,22 +208,4 @@ export class MPhonefield extends ModulVue {
         await this.$nextTick();
         this.$refs.inputMask.focus();
     }
-
-    @Watch('country', { immediate: true })
-    onContryChange(country: Country): void {
-        this.countryModelInternal = country.iso;
-        this.internalCountry = this.countries.find((country: CountryOptions) => country.iso2 === this.countryModelInternal)!;
-        if (!this.as<InputManagement>().internalValue) {
-            this.as<InputManagement>().internalValue = '+' + this.internalCountry.dialCode;
-        }
-
-    }
-
-    @Watch('value', { immediate: true })
-    onValueChanged(value: string): void {
-        if (value) {
-            this.parsePhoneNumber(value);
-        }
-    }
-
 }
