6f52a87816b250eb259c668b594e1af5219817d7
Charles <choc.mah@gmail.com>
2020-08-13 10:24:41 -0400

    add --no-private to jenkins scripts

--- doc/modul-publish-prerelease.groovy
+++ doc/modul-publish-prerelease.groovy
@@ -93,7 +93,7 @@ pipeline {
 				withCredentials([usernamePassword(credentialsId: GIT_CREDS, passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
 					withNPM(npmrcConfig: NPM_CONFIG) {
 						echo "Publish version to npm"
-						sh "yarn run lerna publish ${params.version} --conventional-commits --conventional-prerelease --no-changelog --no-push --preid ${params.prereleaseid}  --dist-tag ${params.disttag} --yes"
+						sh "yarn run lerna publish ${params.version} --conventional-commits --conventional-prerelease --no-changelog --no-push --preid ${params.prereleaseid}  --dist-tag ${params.disttag} --no-private --yes"
 						sh "git push https://${GIT_USERNAME}:${GIT_PASSWORD}@${REPO_URL} --follow-tags"
 					}
 				}
