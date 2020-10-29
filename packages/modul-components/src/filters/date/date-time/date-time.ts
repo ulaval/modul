a9895e9650c38a4cd4934074ff170dd69f4ba51e
cabob <carl.boulianne-boivin@dti.ulaval.ca>
2020-10-29 13:51:20 -0400

    Toujours prendre en compte param withTime (#423)

--- packages/modul-components/src/filters/date/date-time/date-time.ts
+++ packages/modul-components/src/filters/date/date-time/date-time.ts
@@ -1,8 +1,7 @@
 import Vue from 'vue';
 import { FormatMode } from '../../../utils/i18n/i18n';
-import { dateFilter } from '../date/date';
+import { dateFilter, DateFilterParams } from '../date/date';
 import { timeFilter } from '../time/time';
-
 
 export const dateTimeFilter: (date: Date, short?: boolean) => string = (date, short) => {
     const params: any = {
@@ -12,3 +11,13 @@ export const dateTimeFilter: (date: Date, short?: boolean) => string = (date, sh
     return Vue.prototype.$i18n.translate(short ? 'f-m-date-time:short' : 'f-m-date-time:long', params, 0, '', true, FormatMode.Vsprintf);
 };
 
+export class DateTimeFilter {
+    static formatDateTime(date: Date, filterParams?: DateFilterParams): string {
+        const params: any = {
+            date: dateFilter(date, filterParams),
+            time: timeFilter(date)
+        };
+        const shortMode: boolean = !!(filterParams && filterParams.shortMode);
+        return Vue.prototype.$i18n.translate(shortMode ? 'f-m-date-time:short' : 'f-m-date-time:long', params, 0, '', true, FormatMode.Vsprintf);
+    }
+}
