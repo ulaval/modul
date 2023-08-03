675361d258e0c2d01cbcc38027f66e2326a497f0
Thomas BENANCIE <Thomas.benancie.1@ulaval.ca>
2023-08-03 09:40:13 -0400

    Expose m-focus-trap-options

--- packages/modul-components/src/mixins/window-focus-trap/window-focus-trap.ts
+++ packages/modul-components/src/mixins/window-focus-trap/window-focus-trap.ts
@@ -1,26 +1,25 @@
-import { createFocusTrap, FocusTrap } from 'focus-trap';
+import { createFocusTrap, FocusTrap, Options } from 'focus-trap';
 import Component from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
 import { ModulVue } from '../../utils/vue/vue';
-
 @Component
 export class MFocusTrap extends ModulVue {
-    private focusTrap?: FocusTrap;
+    @Prop({ default: () => ({}) })
+    public options!: Options;
 
-    public setFocusTrap(el: HTMLElement, options: {
-        escapeActive?: boolean,
-        initialFocus?: HTMLElement,
-        returnFocusOnDeactivate?: boolean
-    } = {}): void {
-        this.focusTrap = createFocusTrap(el, {
-            escapeDeactivates: options?.escapeActive ?? false,
-            allowOutsideClick: true,
-            initialFocus: options?.initialFocus ?? el,
-            returnFocusOnDeactivate: options?.returnFocusOnDeactivate ?? true,
-            preventScroll: true
-        });
+    public focusTrap?: FocusTrap;
+
+    public setFocusTrap(el: HTMLElement, options: Options = {}): void {
+        options.allowOutsideClick = options.allowOutsideClick ?? true;
+        options.escapeDeactivates = options.escapeDeactivates ?? false;
+        options.initialFocus = options.initialFocus ?? el;
+        options.preventScroll = options.preventScroll ?? true;
+
+        options = { ...options, ...this.options };
+
+        this.focusTrap = createFocusTrap(el, options);
         this.focusTrap.activate();
     }
-
     public removeFocusTrap(): void {
         if (!this.focusTrap) { return; }
         this.focusTrap.deactivate();
