5d3259214bcc2bedea064cd5375477f12cd81d5d
Javier Morato <javiermorato@gmail.com>
2023-09-11 10:17:12 -0400

    Changement lib focus trap (#545)
    * Changement lib focus trap
    
    * fix lint
    
    * fix lint
    
    ---------
    
    Co-authored-by: JavierOrlandoMorato <javier.morato.1@ulaval.ca>

--- packages/modul-components/src/mixins/window-focus-trap/window-focus-trap.ts
+++ packages/modul-components/src/mixins/window-focus-trap/window-focus-trap.ts
@@ -1,4 +1,5 @@
-import { createFocusTrap, FocusTrap, Options } from 'focus-trap';
+import { A11yTabTrap, A11yTabTrapOptions, createTabTrap } from 'a11y-tab-trap';
+import { Options } from 'focus-trap';
 import { Component, Prop } from 'vue-property-decorator';
 import { ModulVue } from '../../utils/vue/vue';
 @Component
@@ -6,29 +7,43 @@ export class MFocusTrap extends ModulVue {
     @Prop({ default: false })
     public readonly focusTrapDisabled: boolean;
 
-    public focusTrap?: FocusTrap;
+    public focusTrap?: A11yTabTrap;
 
-    public setFocusTrap(el: HTMLElement, options: Options = {}): void {
-        if (this.focusTrapDisabled) { return; }
+    public setFocusTrap(
+        el: HTMLElement,
+        options: Partial<Options> | A11yTabTrapOptions = {}
+    ): void {
+        if (this.focusTrapDisabled) {
+            return;
+        }
 
-        options.allowOutsideClick = options.allowOutsideClick ?? true;
-        options.escapeDeactivates = options.escapeDeactivates ?? false;
-        options.initialFocus = options.initialFocus ?? el;
-        options.preventScroll = options.preventScroll ?? true;
+        if (options.initialFocus instanceof HTMLElement) {
+            this.focusTrap = createTabTrap(el, {
+                initialFocus: options.initialFocus
+            });
 
-        this.focusTrap = createFocusTrap(el, options);
-        this.focusTrap.activate();
+            return;
+        }
+
+        this.focusTrap = createTabTrap(el);
     }
     public removeFocusTrap(): void {
-        if (!this.focusTrap || this.focusTrapDisabled) { return; }
+        if (!this.focusTrap || this.focusTrapDisabled) {
+            return;
+        }
 
-        this.focusTrap.deactivate();
+        this.focusTrap.remove();
     }
 
     protected beforeDestroy(): void {
-        if (!this.focusTrap || this.focusTrapDisabled) { return; }
+        if (!this.focusTrap || this.focusTrapDisabled) {
+            return;
+        }
 
-        if (!this.focusTrap) { return; }
-        this.focusTrap.deactivate();
+        if (!this.focusTrap) {
+            return;
+        }
+
+        this.focusTrap.destroy();
     }
 }
