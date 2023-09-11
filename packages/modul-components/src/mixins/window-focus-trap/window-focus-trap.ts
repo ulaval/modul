4ab090dc7cf1e0203163a44d2bc1ac052397da64
JavierOrlandoMorato <javier.morato.1@ulaval.ca>
2023-09-11 09:54:52 -0400

    fix lint

--- packages/modul-components/src/mixins/window-focus-trap/window-focus-trap.ts
+++ packages/modul-components/src/mixins/window-focus-trap/window-focus-trap.ts
@@ -1,49 +1,49 @@
-import { A11yTabTrap, A11yTabTrapOptions, createTabTrap } from "a11y-tab-trap";
-import { Options } from "focus-trap";
-import { Component, Prop } from "vue-property-decorator";
-import { ModulVue } from "../../utils/vue/vue";
+import { A11yTabTrap, A11yTabTrapOptions, createTabTrap } from 'a11y-tab-trap';
+import { Options } from 'focus-trap';
+import { Component, Prop } from 'vue-property-decorator';
+import { ModulVue } from '../../utils/vue/vue';
 @Component
 export class MFocusTrap extends ModulVue {
-  @Prop({ default: false })
-  public readonly focusTrapDisabled: boolean;
+    @Prop({ default: false })
+    public readonly focusTrapDisabled: boolean;
 
-  public focusTrap?: A11yTabTrap;
+    public focusTrap?: A11yTabTrap;
 
-  public setFocusTrap(
-    el: HTMLElement,
-    options: Partial<Options> | A11yTabTrapOptions = {}
-  ): void {
-    if (this.focusTrapDisabled) {
-      return;
+    public setFocusTrap(
+        el: HTMLElement,
+        options: Partial<Options> | A11yTabTrapOptions = {}
+    ): void {
+        if (this.focusTrapDisabled) {
+            return;
+        }
+
+        if (options.initialFocus instanceof HTMLElement) {
+            this.focusTrap = createTabTrap(el, {
+                initialFocus: options.initialFocus,
+            });
+
+            return;
+        }
+
+        this.focusTrap = createTabTrap(el);
+    }
+    public removeFocusTrap(): void {
+        if (!this.focusTrap || this.focusTrapDisabled) {
+            return;
+        }
+
+        this.focusTrap.remove();
     }
 
-    if (options.initialFocus instanceof HTMLElement) {
-      this.focusTrap = createTabTrap(el, {
-        initialFocus: options.initialFocus,
-      });
+    protected beforeDestroy(): void {
+        if (!this.focusTrap || this.focusTrapDisabled) {
+            return;
+        }
 
-      return;
+        if (!this.focusTrap) {
+            return;
+        }
+
+        this.focusTrap.destroy();
     }
-
-    this.focusTrap = createTabTrap(el);
-  }
-  public removeFocusTrap(): void {
-    if (!this.focusTrap || this.focusTrapDisabled) {
-      return;
-    }
-
-    this.focusTrap.remove();
-  }
-
-  protected beforeDestroy(): void {
-    if (!this.focusTrap || this.focusTrapDisabled) {
-      return;
-    }
-
-    if (!this.focusTrap) {
-      return;
-    }
-
-    this.focusTrap.destroy();
-  }
 }
